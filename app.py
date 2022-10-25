00e95798d4dbc66430d8e3e09d6a4ca7c8e353c6
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2022-10-25 15:17:31 +0900

    mini update くだらない

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ def hello_world():
     #text = request.args.get('text', '')
     return render_template("index.html")
 
+# 顔認証のルート
+@app.route('/kao')
+def face_login():
+    # textで指定されたパラメータをJsonに整形して返す
+    #text = request.args.get('text', '')
+    return render_template("index.html")
+
+
+
+#指紋認証のルート
+@app.route('/yubi')
+def finger_login():
+    # textで指定されたパラメータをJsonに整形して返す
+    #text = request.args.get('text', '')
+    return render_template("index.html")
+
 # 外部に公開できるようにポート開放
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000)
