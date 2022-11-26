24047a66e6986540c37306a3fc8451d46f0d0250
Gelimaster <50572505+Gelimaster@users.noreply.github.com>
2022-11-26 11:10:03 +0900

    testpls

--- dbtestconn.py
+++ dbtestconn.py
@@ -1,39 +1,25 @@
-import pymysql
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'gp41_db'
+ 
+mysql = MySQL(app)
 
-# cursor=""
-# conn=""
-def register_user():
-    print("start connection")
-    # mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='root',port=4444,database='gp41_db')
-    # print(mydb)
-    # #establishing the connection
-    connection = pymysql.connect(host="localhost",database="gp41_db",port=4444,user="root",password="root",charset="utf8")
-    # cursor = connection.cursor()
-    # some other statements  with the help of cursor
-    with connection.cursor() as cursor:
-        sql="SELECT * FROM user_table "
-        cursor.execute(sql)
-        resul=cursor.fetchone()
-        print(resul)
-        return True
-    if connection:
-        print(connection)
-        connection.close()
-        return True
-    else:
-        connection.close()
-        print("false")    
-        return False 
-    print("start connect")
-    
-
-    # #Creating a cursor object using the cursor() method
-    cursor = conn.cursor()  
-    print(conn)
+#Creating a cursor object using the cursor() method
+with app.app_context():
+    cursor = mysql.connection.cursor()  
     print("connection estabilished")
+    username = "pppp"
+    mail="gggg@gmail.com"
+    phone="08033445566"
 
-    # conn.close()
-    # # Preparing SQL query to INSERT a record into the database.
+
+    # Preparing SQL query to INSERT a record into the database.
     sql = """INSERT INTO user_table(
     user_name,user_mailaddress,user_phone)
     VALUES ("""+username+""","""+mail+""","""+phone+""")"""
@@ -44,25 +30,10 @@ def register_user():
         cursor.execute(sql)
 
         # Commit your changes in the database
-        conn.commit()
+        mysql.connection.commit()
 
         print("success")
-        return True
 
     except:
-        # Rolling back in case of error
-        conn.rollback()
-
-        # Closing the connection
-        conn.close()
 
         print("error")
-        return False
-
-#"test","rafael@gmail.com","08036666666"
-
-if __name__ == '__main__':
-    print("start def")
-    register_user()
-
-    
